96c734bb9fcf90b2334c649e203c4e384ad612f3
dima.osadchy@gmail.com <dmytro.osadchyy@strikersoft.com>
2017-08-22 21:01:51 +0300

    Phone number screen refactoring

--- Sphaer/PhoneInputViewController.swift
+++ Sphaer/PhoneInputViewController.swift
@@ -12,21 +12,21 @@ import PhoneNumberKit
 class PhoneInputViewController: BaseRegistrationController {
 
     @IBOutlet weak var phoneNumberInputInfoLabel: UILabel!
-    
     @IBOutlet weak var yourCountryPlaceholderLabel: UILabel!
-    
     @IBOutlet weak var emojjiFlagOfCountryLabel: UILabel!
-    
     @IBOutlet weak var countryNameLabel: UILabel!
-    
     @IBAction func selectCountryPressed(_ sender: UIButton) {
     }
     
     @IBOutlet weak var yourPhonePlaceholderLabel: UILabel!
-    
     @IBOutlet weak var countryCodeLabel: UILabel!
-    
     @IBOutlet weak var phoneTextField: PhoneNumberTextField!
+    
+    // Get current locale code
+    //var country = Country.currentCountry
+    
+    // Phone number formatter
+    let phoneNumberKit = PhoneNumberKit()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +35,8 @@ class PhoneInputViewController: BaseRegistrationController {
         
         setupUI()
         setupLabels()
+        
+        self.phoneTextField.becomeFirstResponder()
     }
 
     func setupUI() {
@@ -48,5 +50,8 @@ class PhoneInputViewController: BaseRegistrationController {
         
         emojjiFlagOfCountryLabel.text = "\(Helper.emoji(countryCode: "fi"))"
         countryNameLabel.text = "Finland"
+        
+        phoneTextField.defaultRegion = PhoneNumberKit.defaultRegionCode()
+        phoneTextField.placeholder = "44 7859587"
     }
 }
